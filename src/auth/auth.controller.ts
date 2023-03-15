c10bcf420ace6edd03164965260ae39909ca32b5
Vitor Augusto <vitoraugusto2010201078@gmail.com>
2023-03-15 14:05:11 -0300

    feat: add Upload files

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  FileTypeValidator,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { User } from 'src/decorators/user.decorator';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { UserService } from 'src/user/user.service';
@@ -7,12 +18,14 @@ import { AuthForgetDto } from './dto/auth.forget.dto';
 import { AuthLoginDto } from './dto/auth.login.dto';
 import { AuthRegisterDto } from './dto/auth.register.dto';
 import { AuthResetDto } from './dto/auth.reset.dto';
+import { join } from 'path';
+import { FileService } from 'src/file/file.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(
-    private readonly userService: UserService,
     private readonly authService: AuthService,
+    private readonly fileService: FileService,
   ) {}
   @Post('login')
   async login(@Body() { email, password }: AuthLoginDto) {
@@ -38,5 +51,39 @@ export class AuthController {
   @Post('me')
   async me(@User() user: UserService) {
     return { user };
+  }
+
+  @UseInterceptors(FileInterceptor('file'))
+  @UseGuards(AuthGuard)
+  @Post('photo')
+  async uploadPhoto(
+    @User() user,
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new FileTypeValidator({
+            fileType: 'image/png',
+          }),
+          // new MaxFileSizeValidator({ maxSize: 1024 * 1024 }),
+        ],
+      }),
+    )
+    photo: Express.Multer.File,
+  ) {
+    const path = join(
+      __dirname,
+      '..',
+      '..',
+      'storage',
+      'photos',
+      `photo-${user.id}.png`,
+    );
+    try {
+      this.fileService.upload(photo, path);
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+
+    return { sucess: true };
   }
 }
